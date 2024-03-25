9c45c906d8c83ee52731b4263892a5f4215e5e2a
xzc <xingzhicheng@imudges.com>
2024-03-25 12:00:27 +0800

    拦截器处理

--- src/main/java/com/xzc/interceptor/MyInterceptor2.java
+++ src/main/java/com/xzc/interceptor/MyInterceptor2.java
@@ -14,12 +14,12 @@ public class MyInterceptor2 implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		System.out.println(" 2 preHandle 预处理 ");
-		
-		return false;
+
+		return true;
 	}
-	
+
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
@@ -31,5 +31,5 @@ public class MyInterceptor2 implements HandlerInterceptor {
 			throws Exception {
 		System.out.println(" 2 afterCompletion 页面渲染后处理 ");
 	}
-	
+
 }
